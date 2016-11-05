de93977041160245d2c55bb2d032f4d1d9d37a82
dominikus1993 <dominikus1910@gmail.com>
2016-11-05 19:08:09 +0100

    add OperationStatus enum and IRepository

--- src/Core/Repositories/Interfaces/IRepository.cs
+++ src/Core/Repositories/Interfaces/IRepository.cs
@@ -1,7 +1,20 @@
-﻿namespace Core.Repositories.Interfaces
-{
-    public interface IRepository
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Core.Models;
 
+namespace Core.Repositories.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class, IEntity
+    {
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        IEnumerable<TEntity> FindAll();
+        Task<IEnumerable<TEntity>> FindAllAsync();
+        OperationStatus Add(TEntity entity);
+        OperationStatus AddRange(IEnumerable<TEntity> entities);
+        OperationStatus Remove(TEntity entity);
+        OperationStatus RemoveRange(IEnumerable<TEntity> entities);
     }
 }
