571d1f3e0b745b38febf8547c889ae3cc785654e
dominikus1993 <dominikus1910@gmail.com>
2016-11-05 20:44:55 +0100

    add IsNegate checking

---
+++ src/Core/Utils/Text.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Utils
+{
+    public class Text
+    {
+        public static bool IsNegate(string text)
+        {
+            switch (text.ToLower())
+            {
+                case "not":
+                case "don't":
+                case "dont":
+                case "no":
+                case "nope":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
