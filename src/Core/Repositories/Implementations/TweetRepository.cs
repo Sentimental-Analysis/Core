faafdc9b3aff5a21634e306025b7dc42d0b67c03
dominikus1993 <dominikus1910@gmail.com>
2016-11-05 19:14:23 +0100

    add TweetRepository

---
+++ src/Core/Repositories/Implementations/TweetRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Database.Interfaces;
+using Core.Models;
+using Core.Repositories.Abstractions;
+using Core.Repositories.Interfaces;
+
+namespace Core.Repositories.Implementations
+{
+    public class TweetRepository : Repository<Tweet>, ITweetRepository
+    {
+        public TweetRepository(IDbContext dbManager) : base(dbManager)
+        {
+        }
+    }
+}
