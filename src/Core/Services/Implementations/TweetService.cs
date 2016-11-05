dbf0fe8a76961de50ecae1cdd4fb44e529731b6a
dominikus1993 <dominikus1910@gmail.com>
2016-11-05 21:33:14 +0100

    add unit of work to tweetService

--- src/Core/Services/Implementations/TweetService.cs
+++ src/Core/Services/Implementations/TweetService.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections.Generic;
 using Core.Models;
 using Core.Services.Interfaces;
+using Core.UnitOfWork.Interfaces;
 
 namespace Core.Services.Implementations
 {
     public class TweetService: ITweetService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TweetService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
         public IEnumerable<Tweet> GetTweetByKey(string key)
         {
             throw new System.NotImplementedException();
