f143043067d57ff480e77458af729798095ca266
dominikus1993 <dominikus1910@gmail.com>
2016-11-05 21:18:33 +0100

    add IUnitOFWork

---
+++ src/Core/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Core.Cache.Interfaces;
+using Core.Repositories.Interfaces;
+using Core.Services.Interfaces;
+
+namespace Core.UnitOfWork.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        ITweetRepository Tweets { get; }
+        ICacheService CacheService { get; }
+        ISentimentalAnalysisService SentimentalAnalysis { get; }
+    }
+}
