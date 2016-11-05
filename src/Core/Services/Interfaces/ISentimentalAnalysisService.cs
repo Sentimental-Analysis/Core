e5000c69f2ca984fcaa8231cd852fc9e6fb42f06
dominikus1993 <dominikus1910@gmail.com>
2016-11-05 20:52:07 +0100

    add IService and ISentimentalAnalysis service

---
+++ src/Core/Services/Interfaces/ISentimentalAnalysisService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tweetinvi.Logic;
+
+namespace Core.Services.Interfaces
+{
+    public interface ISentimentalAnalysisService
+    {
+        Task<IEnumerable<Tweet>> AnalyzeAsync(IEnumerable<Tweet> tweets);
+        IEnumerable<Tweet> Analyze(IEnumerable<Tweet> tweets);
+    }
+}
