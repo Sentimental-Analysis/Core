f88281b3ed962fe02a66cefc0a0233f5875b3b5a
dominikus1993 <dominikus1910@gmail.com>
2016-11-05 12:14:45 +0100

    add IDbContext body

--- src/Core/Database/Implementations/IDbContext.cs
+++ src/Core/Database/Implementations/IDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace Core.Database.Implementations
+﻿using System;
+using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Database.Implementations
 {
-    public interface IDbContext
+    public interface IDbContext: IDisposable
     {
+        DbSet<Tweet> Tweets { get; set; }
+        int SaveChanges();
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
     }
 }
