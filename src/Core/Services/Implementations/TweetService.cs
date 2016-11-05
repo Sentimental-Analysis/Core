b59b29e51826ede648bc75badc9f058a5854075b
dominikus1993 <dominikus1910@gmail.com>
2016-11-05 21:15:08 +0100

    add TweetService

---
+++ src/Core/Services/Implementations/TweetService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Core.Models;
+using Core.Services.Interfaces;
+
+namespace Core.Services.Implementations
+{
+    public class TweetService: ITweetService
+    {
+        public IEnumerable<Tweet> GetTweetByKey(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Core/Services/Interfaces/ITweetService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Core.Models;
+
+namespace Core.Services.Interfaces
+{
+    public interface ITweetService : IService
+    {
+        IEnumerable<Tweet> GetTweetByKey(string key);
+    }
+}
