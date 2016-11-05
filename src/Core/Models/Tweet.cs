03fa8e4e331ca11936525a1c01ecdbfcf180ca16
dominikus1993 <dominikus1910@gmail.com>
2016-11-05 12:02:05 +0100

    add tweet class

---
+++ src/Core/Models/Tweet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core.Models
+{
+    public class Tweet
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public string Language { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public int Sentiment { get; set; }
+    }
+}
