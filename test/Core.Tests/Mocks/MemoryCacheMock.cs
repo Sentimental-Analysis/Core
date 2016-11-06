1b4f30529af68b0ef592498dea9abe5d9dcde309
dominikus1993 <dominikus1910@gmail.com>
2016-11-06 16:35:51 +0100

    add UnitOfWorkMock

--- test/Core.Tests/Mocks/MemoryCacheMock.cs
+++ test/Core.Tests/Mocks/MemoryCacheMock.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.Extensions.Caching.Memory;
+﻿using System;
+using System.Collections.Generic;
+using Core.Cache.Interfaces;
 using Moq;
+using Tweetinvi.Logic;
 
 namespace Core.Tests.Mocks
 {
-    public class MemoryCacheMock : IMock<IMemoryCache>
+    public class MemoryCacheMock : IMock<ICacheService>
     {
-        public Mock<IMemoryCache> Mock()
+        public Mock<ICacheService> Mock()
         {
-            var mock = new Mock<IMemoryCache>();
+            var mock = new Mock<ICacheService>();
+            mock.Setup(x => x.GetOrStore(It.IsAny<string>(), It.IsAny<Func<IEnumerable<Tweet>>>(), It.IsAny<TimeSpan>()))
+                .Returns<IEnumerable<Tweet>>(x => x);
             return mock;
         }
     }
--- test/Core.Tests/Mocks/SentimentalAnalysisServiceMock.cs
+++ test/Core.Tests/Mocks/SentimentalAnalysisServiceMock.cs
@@ -1,4 +1,6 @@
-﻿using Core.Services.Interfaces;
+﻿using System.Collections.Generic;
+using Core.Models;
+using Core.Services.Interfaces;
 using Moq;
 
 namespace Core.Tests.Mocks
@@ -8,6 +10,7 @@ namespace Core.Tests.Mocks
         public Mock<ISentimentalAnalysisService> Mock()
         {
             var mock = new Mock<ISentimentalAnalysisService>();
+            mock.Setup(x => x.Analyze(It.IsAny<IEnumerable<Tweet>>())).Returns<IEnumerable<Tweet>>(x => Result<IEnumerable<Tweet>>.Wrap(x));
             return mock;
         }
     }
