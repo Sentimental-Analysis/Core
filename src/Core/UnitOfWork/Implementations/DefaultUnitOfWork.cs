f05676d5a090e246adaccee237cc347fdcbac799
dominikus1993 <dominikus1910@gmail.com>
2016-11-06 09:03:58 +0100

    small change

--- src/Core/UnitOfWork/Implementations/DefaultUnitOfWork.cs
+++ src/Core/UnitOfWork/Implementations/DefaultUnitOfWork.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace Core.UnitOfWork.Implementations
 {
-    public sealed class DefaultUnitOfWork : IUnitOfWork
+    public class DefaultUnitOfWork : IUnitOfWork
     {
         private readonly IDbContext _dbContext;
         private bool _shouldBeDisposed = true;
