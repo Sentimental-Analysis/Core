0d808167f27e17eafaaf6664b3bd8526723ee8b8
dominikus1993 <dominikus1910@gmail.com>
2016-11-06 16:22:44 +0100

    add UnitOfWorkMock

--- test/Core.Tests/Mocks/UnitOfWorkMock.cs
+++ test/Core.Tests/Mocks/UnitOfWorkMock.cs
@@ -1,10 +1,28 @@
-﻿using Core.UnitOfWork.Interfaces;
+﻿using Core.Repositories.Interfaces;
+using Core.Services.Interfaces;
+using Core.UnitOfWork.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
 using Moq;
 
 namespace Core.Tests.Mocks
 {
     public class UnitOfWorkMock : IMock<IUnitOfWork>
     {
+
+        private readonly ITweetRepository _apiTweetRepository;
+        private readonly ITweetRepository _dbTweetRepository;
+        private readonly IMemoryCache _memoryCache;
+        private readonly ISentimentalAnalysisService _sentimentalAnalysisService;
+
+        public UnitOfWorkMock(ITweetRepository apiTweetRepository, ITweetRepository dbTweetRepository, IMemoryCache memoryCache, ISentimentalAnalysisService sentimentalAnalysisService)
+        {
+            _apiTweetRepository = apiTweetRepository;
+            _dbTweetRepository = dbTweetRepository;
+            _memoryCache = memoryCache;
+            _sentimentalAnalysisService = sentimentalAnalysisService;
+        }
+
+
         public Mock<IUnitOfWork> Mock()
         {
             var mock = new Mock<IUnitOfWork>();
