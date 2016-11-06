8a2ae7852aa8134fe2404c0f981f77648d90aeb5
dominikus1993 <dominikus1910@gmail.com>
2016-11-06 16:19:45 +0100

    add UnitOfWorkMock

---
+++ test/Core.Tests/Mocks/UnitOfWorkMock.cs
@@ -0,0 +1,14 @@
+﻿using Core.UnitOfWork.Interfaces;
+using Moq;
+
+namespace Core.Tests.Mocks
+{
+    public class UnitOfWorkMock : IMock<IUnitOfWork>
+    {
+        public Mock<IUnitOfWork> Mock()
+        {
+            var mock = new Mock<IUnitOfWork>();
+            return mock;
+        }
+    }
+}
