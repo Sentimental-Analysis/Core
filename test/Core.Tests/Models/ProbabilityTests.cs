50f77dbe906a696fc6038f134d39867596ac2aa6
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-21 23:03:46 +0100

    add Test for count probability method

---
+++ test/Core.Tests/Models/ProbabilityTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Bayes.Data;
+using FluentAssertions;
+using Xunit;
+
+namespace Core.Tests.Models
+{
+    public class ProbabilityTests
+    {
+        [Fact]
+        public void Count_Pobability_When_All_Words_Are_Negative()
+        {
+            var dict = new Dictionary<string, int>()
+            {
+                {"abandon", -2 },
+                {"abandoned", -2 }
+            };
+
+            var probability = Probability.Count(dict);
+            probability.PositiveProbability.Should().BeLessOrEqualTo(double.Epsilon);
+            probability.NegativeProbability.Should().BeLessOrEqualTo(1);
+        }
+
+        [Fact]
+        public void Count_Pobability_When_All_Words_Are_Positive()
+        {
+            var dict = new Dictionary<string, int>()
+            {
+                {"exuberant", 4 },
+                {"fun", 4 }
+            };
+
+            var probability = Probability.Count(dict);
+            probability.NegativeProbability.Should().BeLessOrEqualTo(double.Epsilon);
+            probability.PositiveProbability.Should().BeLessOrEqualTo(1);
+        }
+
+        [Fact]
+        public void Count_Pobability()
+        {
+            var dict = new Dictionary<string, int>()
+            {
+                {"exuberant", 4 },
+                {"fun", 4 },
+                {"abandon", -2 },
+                {"abandoned", -2 }
+            };
+
+            var probability = Probability.Count(dict);
+            probability.NegativeProbability.Should().BeLessOrEqualTo(0.5);
+            probability.PositiveProbability.Should().BeLessOrEqualTo(0.5);
+        }
+    }
+}
