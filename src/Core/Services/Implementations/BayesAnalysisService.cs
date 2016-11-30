2be83af327092e42852c6562b870fe42633dbcc1
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-30 18:47:15 +0100

    add Bayes Service

---
+++ src/Core/Services/Implementations/BayesAnalysisService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Models;
+using Core.Services.Interfaces;
+
+namespace Core.Services.Implementations
+{
+    public class BayesAnalysisService : ISentimentalAnalysisService
+    {
+        public Task<Result<IEnumerable<Tweet>>> AnalyzeAsync(IEnumerable<Tweet> tweets)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Result<IEnumerable<Tweet>> Analyze(IEnumerable<Tweet> tweets)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
