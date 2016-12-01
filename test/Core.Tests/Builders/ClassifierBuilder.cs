017d60b9d4e117e445998535f4dbf1d74bf17f09
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-01 21:04:14 +0100

    small changes

---
+++ test/Core.Tests/Builders/ClassifierBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Immutable;
+using Bayes.Classifiers.Interfaces;
+using Bayes.Data;
+using Bayes.Learner.Interfaces;
+
+namespace Core.Tests.Builders
+{
+    public class ClassifierBuilder
+    {
+        private IClassifier<Score, string> _classifier;
+        private ILearner<Sentence, LearnerState> _learner;
+        public ClassifierBuilder WithLearnData(ImmutableDictionary<string, int> words)
+        {
+
+        }
+
+
+    }
+}
