8b60aabd4866280f5dcf1aea374877f5b521576e
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-02 21:58:11 +0100

    add AnalysisScore

---
+++ src/Core/Models/AnalysisScore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Bayes.Data;
+
+namespace Core.Models
+{
+    public class AnalysisScore
+    {
+        public WordCategory Sentiment { get; set; }
+        public int PositiveTweetsQuantity { get; set; }
+        public int NegativeTweetsQuantity { get; set; }
+        public IEnumerable<string> KeyWords { get; set; }
+    }
+}
