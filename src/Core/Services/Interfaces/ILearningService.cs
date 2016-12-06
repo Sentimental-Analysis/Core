8e91d55e9753157de9e5cb5bbe3249bba0502670
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-06 16:43:58 +0100

    add ILearningService

---
+++ src/Core/Services/Interfaces/ILearningService.cs
@@ -0,0 +1,11 @@
+﻿using Bayes.Data;
+
+namespace Core.Services.Interfaces
+{
+    public interface ILearningService
+    {
+        LearnerState GetOrStore(LearnerState state);
+        LearnerState Store(LearnerState state);
+        LearnerState Update(LearnerState state);
+    }
+}
