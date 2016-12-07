364bb43302dd0c3693f9055df313597fbe1f72f3
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-07 10:22:36 +0100

    add CacheService stub

--- test/Core.Tests/Services/LearningServiceTests.cs
+++ test/Core.Tests/Services/LearningServiceTests.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using Core.Services.Implementations;
+using static Core.Tests.Builders.LearnStateBuilder;
+using Core.Tests.Builders;
+using Xunit;
 
 namespace Core.Tests.Services
 {
@@ -7,7 +10,12 @@ namespace Core.Tests.Services
         [Fact]
         public void Test_First_Get_Learn_State()
         {
+            var learningService = new LearningService()
+        }
 
+        public static T A<T>(IBuilder<T> stateBuilder)
+        {
+            return stateBuilder.Build();
         }
     }
 }
