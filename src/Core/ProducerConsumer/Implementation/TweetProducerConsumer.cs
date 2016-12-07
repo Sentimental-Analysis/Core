4534b0304471e4c7895064f00bc5c0fb19359682
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-07 16:17:17 +0100

    add TweetProducerConsumer

---
+++ src/Core/ProducerConsumer/Implementation/TweetProducerConsumer.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Core.Models;
+using Core.ProducerConsumer.Interfaces;
+
+namespace Core.ProducerConsumer.Implementation
+{
+    public class TweetProducerConsumer : IProducerConsumer<Tweet>
+    {
+        public Task ProduceAsync(ITargetBlock<Tweet> target)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TConsume> ConsumeAsync<TConsume>(ISourceBlock<Tweet> source)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
