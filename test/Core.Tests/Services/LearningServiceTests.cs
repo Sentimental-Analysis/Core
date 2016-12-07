fd788d4dfd06320fedfff1868bc1f493b58cd8fc
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-07 09:53:38 +0100

    add LearnStateBuilder

---
+++ test/Core.Tests/Services/LearningServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Core.Tests.Services
+{
+    public class LearningServiceTests
+    {
+        [Fact]
+        public void Test_First_Get_Learn_State()
+        {
+
+        }
+    }
+}
