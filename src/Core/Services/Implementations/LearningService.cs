ffb9c66fe74b99057ae8e957dd87614512a41972
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-07 09:38:46 +0100

    add implementation of learning service

---
+++ src/Core/Services/Implementations/LearningService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Bayes.Data;
+using Bayes.Learner.Interfaces;
+using Core.Cache.Interfaces;
+using Core.Services.Interfaces;
+using System.Linq;
+
+namespace Core.Services.Implementations
+{
+    public class LearningService : ILearningService
+    {
+        private readonly ICacheService _cacheService;
+        private readonly string _learnStateCacheKey = $"{nameof(LearningService)}-learnstate";
+        private readonly IEnumerable<Sentence> _sentences;
+        private readonly ITweetLearner _learner;
+
+        public LearningService(ICacheService cacheService, ITweetLearner learner, IEnumerable<Sentence> initSentences)
+        {
+            _cacheService = cacheService;
+            _sentences = initSentences;
+            _learner = learner;
+        }
+
+
+        public LearnerState Get()
+        {
+            return _cacheService.GetOrStore(_learnStateCacheKey, () =>
+            {
+                return _sentences.Aggregate(LearnerState.Empty, (state, sentence) => _learner.Learn(state, sentence));
+            }, TimeSpan.FromDays(1));
+        }
+
+        public LearnerState Learn(IEnumerable<Sentence> sentences)
+        {
+            var oldState = Get();
+            _cacheService.Clear(_learnStateCacheKey);
+            return _cacheService.GetOrStore(_learnStateCacheKey, () =>
+            {
+                return sentences.Aggregate(oldState, (state, sentence) => _learner.Learn(state, sentence));
+            }, TimeSpan.FromDays(1));
+        }
+    }
+}
