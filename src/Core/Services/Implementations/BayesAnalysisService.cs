42a3a550df1bf1503d69b6faa9ecf09c6112eec9
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-08 13:49:20 +0100

    small changes

--- src/Core/Services/Implementations/BayesAnalysisService.cs
+++ src/Core/Services/Implementations/BayesAnalysisService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Bayes.Classifiers.Implementations;
 using Bayes.Classifiers.Interfaces;
 using Bayes.Data;
 using Core.Models;
@@ -12,10 +15,12 @@ namespace Core.Services.Implementations
     public class BayesAnalysisService : ISentimentalAnalysisService
     {
         private readonly IClassifier<Score, string> _tweetClassifier;
+        private readonly ILearningService _learningService;
 
-        public BayesAnalysisService(IClassifier<Score, string> tweetClassifier)
+        public BayesAnalysisService(IClassifier<Score, string> tweetClassifier, ILearningService learningService)
         {
             _tweetClassifier = tweetClassifier;
+            _learningService = learningService;
         }
 
         public Task<Result<IEnumerable<Tweet>>> AnalyzeAsync(IEnumerable<Tweet> tweets)
@@ -25,6 +30,13 @@ namespace Core.Services.Implementations
 
         public Result<IEnumerable<Tweet>> Analyze(IEnumerable<Tweet> tweets)
         {
+            var buffer = new BufferBlock<Tweet>(new DataflowBlockOptions(){ BoundedCapacity = Environment.ProcessorCount * 5});
+            var classifyTransformBlock = new TransformBlock<Tweet, Tuple<Tweet, LearnerState>>(tweet =>
+            {
+                var learnState = _learningService.Get();
+                var score = new TweetClassifier(learnState).Classify(tweet.Text);
+                return Tuple.Create(tweet.WithNewSentiment(score.Sentence.Category), learnState);
+            });
             var result = tweets.AsParallel().WithDegreeOfParallelism(Environment.ProcessorCount).Select(x =>
             {
                 var res = _tweetClassifier.Classify(x.Text);
