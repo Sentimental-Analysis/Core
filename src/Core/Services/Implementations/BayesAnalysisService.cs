9248c8af02c3d0d8aa6ccd05ac4383713df00a5f
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-12-15 10:56:27 +0100

    refactoring

--- src/Core/Services/Implementations/BayesAnalysisService.cs
+++ src/Core/Services/Implementations/BayesAnalysisService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using Bayes.Classifiers.Interfaces;
